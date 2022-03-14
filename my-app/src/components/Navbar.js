8303e89f635ea81778502d62ca7f85a842b9c280
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-14 16:31:05 -0300

    home

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+    
     return (
         <>
           <nav className="navbar">
@@ -10,9 +15,16 @@ function Navbar() {
                   <Link to="/" className="navbar-logo">
                       TRVL <i className="fab fa-typo3"/>
                   </Link>
-                  <div className='menu-icon'>
+                  <div className='menu-icon' onClick={handleClick}>
                       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                   </div>
+                  <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                      <li className='nav-item'>
+                          <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                              Home
+                          </Link>
+                      </li>
+                  </ul>
               </div>
           </nav>
         </>
