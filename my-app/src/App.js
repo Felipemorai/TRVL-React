99ad88da1e675d47e908bba6d91b015412436393
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-14 15:49:15 -0300

    Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Come on hotties, let's get it!</h1>
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
@@ -6,7 +7,7 @@ function Navbar() {
           <nav className="navbar">
               <div className="navbar-container">
                   <Link to="/" className="navbar-logo">
-                      TRVL
+                      TRVL <i className="fab fa-typo3"/>
                   </Link>
               </div>
           </nav>
