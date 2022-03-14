c032a20c013b4b9fefb98a03f3f3b79a21155e8c
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-14 16:18:25 -0300

    icon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
