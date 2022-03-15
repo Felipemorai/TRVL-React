06ecb0f1353b21232797d96bf7cb463d509ace25
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-15 16:00:31 -0300

    links

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -24,6 +24,21 @@ function Navbar() {
                               Home
                           </Link>
                       </li>
+                      <li className='nav-item'>
+                          <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                              Services
+                          </Link>
+                      </li>
+                      <li className='nav-item'>
+                          <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                              Products
+                          </Link>
+                      </li>
+                      <li className='nav-item'>
+                          <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                              Sign Up
+                          </Link>
+                      </li>
                   </ul>
               </div>
           </nav>
