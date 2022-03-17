f04cd3b92d7dd57f053db168b22292fe4c852ffa
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-17 09:33:37 -0300

    showButton

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,10 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        }
+        else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton)
     
     return (
         <>
@@ -40,6 +52,7 @@ function Navbar() {
                           </Link>
                       </li>
                   </ul>
+                  {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
               </div>
           </nav>
         </>
