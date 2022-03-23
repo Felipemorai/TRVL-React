b35d6ff3c79cd74371de17dab99b8718db9acdb4
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-23 09:16:38 -0300

    upgrade

--- my-app/src/components/CardItem.js
+++ my-app/src/components/CardItem.js
@@ -7,7 +7,7 @@ function CardItem() {
           <li className="card__item">
               <Link className="cards__item__link">
                   <figure className="cards__item__pic-wrap">
-                      <img src="/" alt="Travel" 
+                      <img src="/" alt="Travel Image" 
                       className="cards__item__img" />
                   </figure>
                   <div className='cards__item__info'>
--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from './CardItem.js';
+import './Cards.css';
 
 function Cards() {
     return (
