a354b3a4ddbf0d16fd2f3e548a1bf0c805daf9af
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-03-23 10:02:50 -0300

    props

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -9,7 +9,13 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                          src="images/img-9.jpg"
+                          text="Explore the hidden waterfall deep
+                          inside the Amazon Jungle"
+                          label='Adventure'
+                          path='/services'
+                        />
                     </ul>
                 </div>
             </div>
